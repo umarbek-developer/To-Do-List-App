2777f3ffd1a222b495daeb7cdcf3c426c94ff711
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-04-29 17:33:57 +0500

    Hammasini tugirladim, tekshirib kuring, Serializers, views, urls, va pythonanywhere ni xam tugirlayapman

--- src/apps/todo_list/resources.py
+++ src/apps/todo_list/resources.py
@@ -1,18 +1,8 @@
-from import_export import resources, fields
-from import_export.widgets import ForeignKeyWidget
-from django.contrib.auth import get_user_model
+from import_export import resources
 from .models import Todo
-
-User = get_user_model()
 
 
 class TodoResource(resources.ModelResource):
-    user = fields.Field(
-        column_name="user",
-        attribute="user",
-        widget=ForeignKeyWidget(User, field="email"),
-    )
-
     class Meta:
         model = Todo
         fields = (
